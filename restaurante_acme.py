978089fed13001177a36988c4503b2b296dc03d6
ninistrellita <carrillogonzalezbrendanicol@gmail.com>
2026-04-09 14:22:26 -0500

    Add files via upload

---
+++ restaurante_acme.py
@@ -0,0 +1,34 @@
+from funciones import crear_producto, crear_mesa, crear_cliente, registro_venta, crear_factura
+while True:
+    print("---------------------------------------")
+    print("SISTEMA DE FACRURACION RESTAURANTE ACME")
+    print("1. Ver/agregar productos")
+    print("2. Ver/agregar mesas")
+    print("3. Ver/registar clientes")
+    print("4. Crear factura")
+    print("5. Registro de ventas")
+    print("6. Salir")
+    print("---------------------------------------")
+    opcion = input("Digite la opcion: ")
+    print("---------------------------------------")
+
+    if opcion == "1":
+        crear_producto()
+
+    elif opcion == "2":
+        crear_mesa()
+
+    elif opcion == "3":
+        crear_cliente()
+
+    elif opcion == "4":
+        crear_factura()
+
+    elif opcion == "5":
+        registro_venta()
+
+    elif opcion == "6":
+        print("Saliendo del sistema...")
+        break
+    else:
+        print("Opcion no valida")
